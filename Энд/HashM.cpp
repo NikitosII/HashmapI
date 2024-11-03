8c2b8c7056593f5314c95999c107c93dd51ee09a
NikitosII <160042225+NikitosII@users.noreply.github.com>
2024-11-03 20:49:20 +0300

    Update HashM.cpp

--- Энд/HashM.cpp
+++ Энд/HashM.cpp
@@ -21,7 +21,7 @@ Hashmap::~Hashmap() {
 	items = nullptr;
 }
 
-
+// хэш-функция 
 long Hashmap::hashFunc(std::string data) {
 	long sum = 0;
 	for (int i = 0; i < data.length(); i++) {
@@ -30,7 +30,7 @@ long Hashmap::hashFunc(std::string data) {
 	return (sum % size);
 }
 
-
+// добавление 
 void Hashmap::insertItem(std::string key, std::string data, std::string key2) {
 
 	int index = hashFunc(data);
@@ -55,7 +55,7 @@ void Hashmap::insertItem(std::string key, std::string data, std::string key2) {
 	}
 }
 
-
+// нахождение по полной data
 std::string Hashmap::getItem(std::string data) {
 	int index = hashFunc(data);
 	Item* current = items[index];
@@ -68,7 +68,7 @@ std::string Hashmap::getItem(std::string data) {
 	return " "; 
 }
 
-
+// удаление data
 void Hashmap::deleteItem(std::string data) {
 	long index = hashFunc(data);
 
@@ -91,7 +91,7 @@ void Hashmap::deleteItem(std::string data) {
 	}
 }
 
-
+// вывод всего 
 void Hashmap::displayHashmap() {
 	for (int i = 0; i < size; i++) {
 		Item* current = items[i];
@@ -104,6 +104,7 @@ void Hashmap::displayHashmap() {
 	}
 }
 
+// проверка data
 bool Hashmap::isValidData(const std::string& data) {
 	if (data.length() != 5 || !std::all_of(data.begin(), data.end(), ::isdigit)) {
 		return false;
@@ -112,6 +113,7 @@ bool Hashmap::isValidData(const std::string& data) {
 	return true;
 }
 
+// проверка имя
 bool Hashmap::isValidKey(const std::string& key) {
 
 	for (char c : key) {
@@ -122,10 +124,47 @@ bool Hashmap::isValidKey(const std::string& key) {
 	return true;
 }
 
-
+// проверка даты 
 bool Hashmap::isValidKey2(const std::string& key2) {
-	std::regex pattern("^\\d{2}\\.\\d{2}\\.\\d{4}$");
-	return std::regex_match(key2, pattern);
+	std::regex pattern("^(\\d{2})\\.(\\d{2})\\.(\\d{4})$"); // формат даты 
+	std::smatch matches; // объект используется для хранения результатов сопоставления регулярного выражения с key2
+
+	if (!std::regex_match(key2, matches, pattern)) {
+		return false;
+	} // если совпадает, передаём в matches 
+
+	int day = std::stoi(matches[1]);
+	int month = std::stoi(matches[2]);
+	int year = std::stoi(matches[3]);
+
+	// Проверка на високосный 
+	bool LeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+	// номер месяца проверка 
+	if (month < 1 || month > 12) {
+		return false;
+	}
+
+	int days_month;
+	switch (month) {
+	case 2:
+		days_month = LeapYear ? 29 : 28;
+		break;
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		days_month = 30;
+		break;
+	default:
+		days_month = 31;
+	}
+	// проверки на номер дня 
+	if (day < 1 || day > days_month) {
+		return false;
+	}
+
+	return true;
 }
 
 
@@ -136,87 +175,91 @@ void Hashmap::menu() {
 	std::string key, data, key2;
 
 	do {
-		std::cout << "•1 Äîáàâëåíèå çàïèñè \n";
-		std::cout << "•2 Ïîèñê\n";
-		std::cout << "•3 Óäàëåíèå çàïèñè\n";
-		std::cout << "•4 Ïðîñìîòð âñåõ èìåþùèõñÿ çàïèñåé\n";
-		std::cout << "•5 Ïîèñê ïî ôðàãìåíòó\n";
-		std::cout << "•0 Âûõîä\n";
-		std::cout << "Ââåäèòå íîèåð ïóíêòà(0-5): ";
+		std::cout << "•1 Добавление записи \n";
+		std::cout << "•2 Поиск\n";
+		std::cout << "•3 Удаление записи\n";
+		std::cout << "•4 Просмотр всех имеющихся записей\n";
+		std::cout << "•5 Поиск по фрагменту\n";
+		std::cout << "•0 Выход\n";
+		std::cout << "Введите номер пункта(0-5): ";
 		std::cin >> choice;
 
 		switch (choice) {
-		case 1:
+		case 1: {
 			if (Level >= size) {
-				std::cout << "Õýø-òàáëèöà ïåðåïîëíåíà.\n";
+				std::cout << "Хэш-таблица переполнена.\n";
 				system("pause");
+				system("cls");
 			}
 			else {
 
-				while (true) {
-					std::cout << "Enter ÔÈÎ: ";
+				do {
+					std::cout << "Enter ФИО: ";
 					std::cin.ignore();
 					std::getline(std::cin, key);
 
-					do {
-						std::cout << "Enter ФИО: ";
-						std::cin.ignore();
-						std::getline(std::cin, key);
+				} while (!isValidKey(key));
 
-					} while (!isValidKey(key));
+				do {
+					std::cout << "Enter номер договора (из 5 цифр): ";
+					std::cin >> data;
 
-					do {
-						std::cout << "Enter номер договора (из 5 цифр): ";
-						std::cin >> data;
+				} while (!isValidData(data));
 
-					} while (!isValidData(data));
+				do {
+					std::cout << "Enter др (дд.мм.гггг): ";
+					std::cin >> key2;
 
-					do {
-						std::cout << "Enter др (дд.мм.гггг): ";
-						std::cin >> key2;
+				} while (!isValidKey2(key2));
 
-					} while (!isValidKey2(key2));
 
-					insertItem(key, data, key2);
-					Level++;
-					break; 
-				}
+				// Если все данные корректны, вставляем 
+				insertItem(key, data, key2);
+				Level++;
 
 			}
 			system("cls");
-
 			break;
+		}
 		case 2:
-			std::cout << "Ââåäèòå äàííûå äëÿ ïîèñêà: ";
+
+			std::cout << "Введите данные для поиска: ";
 			std::cin >> data;
-			std::cout << "Ðåçóëüòàò : " << getItem(data) << std::endl;
+			std::cout << "Результат : " << getItem(data) << std::endl;
 			system("pause");
 			system("cls");
 			break;
+
 		case 3:
-			std::cout << "Ââåäèòå äàííûå äëÿ óäàëåíèÿ ( íîìåð äîãîâîðà ): ";
+
+			std::cout << "Введите данные для удаления ( номер договора ): ";
 			std::cin >> data;
 			deleteItem(data);
 			Level--;
 			system("cls");
 			break;
+
 		case 4:
+
 			displayHashmap();
 			system("pause");
 			system("cls");
 			break;
+
 		case 5: {
-			std::string key2;
-			std::cin >> key2;
-			std::vector<std::string> results = search(key2);
+
+			std::string key3;
+			std::cin >> key3;
+			std::vector<std::string> results = search(key3);
+
 			if (!results.empty()) {
-				std::cout << "Íàéäåííûå çàïèñè:\n";
+				std::cout << "Найденные записи:\n";
 				for (const std::string& result : results) {
 					std::cout << result << std::endl;
 				}
 			}
 			else {
-				std::cout << "Íè÷åãî íå íàéäåíî.\n";
+				std::cout << "Ничего не найдено.\n";
 			}
 			system("pause");
 			system("cls");
@@ -226,7 +269,7 @@ void Hashmap::menu() {
 		case 0:
 			break;
 		default:
-			std::cout << "Íåâåðíûé âûáîð. \n";
+			std::cout << "Неверный выбор. \n";
 			system("pause");
 			system("cls");
 			break;
@@ -235,7 +278,7 @@ void Hashmap::menu() {
 }
 
 
-
+// вычисляет префиксные функции
 std::vector<int> Hashmap::prefix_function(const std::string& pattern) {
 	int n = pattern.length();
 	std::vector<int> pi(n, 0);
@@ -252,31 +295,31 @@ std::vector<int> Hashmap::prefix_function(const std::string& pattern) {
 	return pi;
 }
 
-
+Поиск подстроки
 std::vector<int> Hashmap::KMPSearch(const std::string& text, const std::string& pattern) {
 	std::vector<int> positions;
 	std::vector<int> pi = prefix_function(pattern);
-	int n = text.length();
-	int m = pattern.length();
+	int n = text.length(); // Длина текста
+	int m = pattern.length();// Длина шаблона
 	int q = 0;
 	for (int i = 0; i < n; ++i) {
 		while (q > 0 && pattern[q] != text[i]) {
-			q = pi[q - 1]; 
+			q = pi[q - 1];  // Возвращаемся к началу шаблона для поиска следующего вхождения
 		}
-		if (pattern[q] == text[i]) { 
+		if (pattern[q] == text[i]) { // Если символы совпадают, увеличиваем q
 			++q;
 		}
-		if (q == m) {
-			positions.push_back(i - m + 1); 
-			q = pi[q - 1];
+		if (q == m) { // Если весь шаблон совпал 
+			positions.push_back(i - m + 1); // Добавляем позицию вхождения в вектор позиций 
+			q = pi[q - 1]; 
 		}
 	}
 	return positions;
 }
 
-
+// ищет все записи в хэш - таблице
 std::vector<std::string> Hashmap::search(const std::string& fragment) {
-	std::vector<std::string> results; 
+	std::vector<std::string> results; // Вектор для хранения найденных
 	for (int i = 0; i < size; ++i) {
 		if (items[i] != nullptr) {
 			std::vector<int> positions = KMPSearch(items[i]->data, fragment);
